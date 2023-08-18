1e5862f82036a993917db6a54ca408c70f000d5a
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-18 16:46:43 +0530

    123

--- src/test/java/demo_project_practice/Test_demo.java
+++ src/test/java/demo_project_practice/Test_demo.java
@@ -37,7 +37,11 @@ public class Test_demo {
 		  System.out.println("edge launch");
 		  break;
 		  
-		 
+		 case "safari":
+			  //System.setProperty("webdriver.edge.driver", "C:\\Users\\My_Work\\Desktop\\Villeos_project\\cicdpractice\\driver\\msedgedriver.exe");
+			  driver=new SafariDriver();
+			  System.out.println("edge launch");
+			  break;
 		  
 		  default:
 		  System.out.println("not recomend");
